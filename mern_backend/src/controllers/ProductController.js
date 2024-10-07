9c0c30ac5d7e9d58ab96ef94f4c1dda0bcb2872a
MgnouC <Cuong2003pto@gmail.com>
2024-10-07 20:43:49 +0700

    commit
    add sort and querry

--- mern_backend/src/controllers/ProductController.js
+++ mern_backend/src/controllers/ProductController.js
@@ -84,7 +84,9 @@ const deleteProduct = async (req,res)  => {
 
 const getAllProduct = async (req,res)  => {
     try {
-        const response = await ProductService.getAllProduct()
+        const {limit, page, sort, filter}  = req.query
+        const response = await ProductService.getAllProduct(Number(limit) || 8 , Number(page) || 0, sort, filter)
+
         return res.status(200).json(response)
 
     } catch(e){
--- mern_backend/src/services/ProductService.js
+++ mern_backend/src/services/ProductService.js
@@ -89,15 +89,46 @@ const deleteProduct = (id) => {
     });
 };
 
-const getAllProduct = () => {
+const getAllProduct = (limit,  page, sort, filter) => {
     return new Promise(async (resolve, reject) => {
         try {
-            //const checkUser = await User.findOne({ _id: id })
-            const allProduct =  await Product.find().sort({ createdAt: -1 });
+            const totalProduct =  await Product.countDocuments();
+            if (filter) {
+                const label = filter[0]
+                const allObjectFilter = await Product.find({ [label]: {'$regex': filter[1]}})
+                resolve({
+                        status: 'OK',
+                        message: 'Success',
+                        data: allObjectFilter,
+                        total: totalProduct,  // Total count from earlier
+                        pageCurrent: Number(page + 1),
+                        totalPage: Math.ceil(allObjectFilter / limit)
+                    });
+                }
+            
+            if(sort){
+                const  productsSoft = {}
+                productsSoft[sort[1]]= sort[0]
+                const allProductSort =  await Product.find().skip(page * limit).limit(limit).sort(productsSoft);
+                const totalSoftProducts = await Product.countDocuments(productsFilter);               
+
+                resolve  ({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    data: allProductSort,
+                    total:  totalSoftProducts,
+                    pageCurrtent: Number(page + 1),
+                    totalPage: Math.ceil(allProductSort / limit) 
+                })
+            }
+            const allProduct =  await Product.find().skip(page * limit).limit(limit)
             resolve  ({
                 status: 'OK',
                 message: 'SUCCESS',
-                data: allProduct
+                data: allProduct,
+                total:  totalProduct,
+                pageCurrtent: Number(page + 1),
+                totalPage: Math.ceil(totalProduct / limit) 
             })
             
         } catch (e) {
